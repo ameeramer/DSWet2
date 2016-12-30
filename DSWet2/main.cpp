fd47ffa36c9d339aeef0085cdc41effdc486c179
Ameer Amer <ameeramer@campus.technion.ac.il>
2016-12-30 23:46:47 +0200

    Initial Commit

---
+++ DSWet2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSWet2
+//
+//  Created by Ameer Amer on 30/12/2016.
+//  Copyright © 2016 Ameer Amer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
