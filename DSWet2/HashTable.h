36aecdff4404394ea2adef6f1140cfcf513e1f12
Ameer Amer <ameeramer@campus.technion.ac.il>
2017-01-01 14:20:20 +0200

    started implementing hashTable.

---
+++ DSWet2/HashTable.h
@@ -0,0 +1,73 @@
+//
+//  HashTable.h
+//  DSWet2
+//
+//  Created by Ameer Amer on 31/12/2016.
+//  Copyright © 2016 Ameer Amer. All rights reserved.
+//
+
+#ifndef HashTable_h
+#define HashTable_h
+
+template <class T>
+struct ListNode {
+    int key;
+    T* data;
+    struct ListNode* next;
+};
+
+template <class T>
+class List {
+    struct ListNode* head;
+    
+public:
+    List(){
+        head = new struct ListNode;
+        head->key = -1;
+        head->data = null;
+        head->next = null;
+    }
+    
+    void Insert(int key, T* data){
+        struct ListNode* newNode = new struct ListNode;
+        newNode->key = key;
+        newNode->data = data;
+        newNode->next = head->next;
+        head->next = newNode;
+    }
+    
+    void deleteElement(int key){
+        struct ListNode* curr = head->next;
+        while(curr->next){
+            if(curr->next->key = key){
+                struct ListNode* toDelete = curr->next;
+                curr->next = toDelte->next;
+                delete (toDelete);
+            }
+            curr = curr->next;
+        }
+    }
+    
+    T* getElement(int key){
+        struct ListNode* curr = head->next;
+        while(curr){
+            if(curr->key = key){
+                return curr->data;
+            }
+            curr = curr->next;
+        }
+        return null;
+    }
+};
+
+template <class T>
+class DynamicHashTable {
+    List* array;
+    int size;
+    
+    static void multiplySize {
+        
+    }
+};
+
+#endif /* HashTable_h */
